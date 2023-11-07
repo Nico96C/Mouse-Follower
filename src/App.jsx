57685887d1eb0133e5267f5c26f03e317e7ca88d
Nico96C <nicolas.cuello96@hotmail.com>
2023-11-07 14:42:36 -0300

    Mouse Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,59 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('efecto', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    } //Se usara para limpiar el EventListener!
+
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        background: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} tracking
+      </button>
+    </>
+  )
+}
 
 function App() {
 
+
   return (
-   <h3> Proyecto 3 </h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
